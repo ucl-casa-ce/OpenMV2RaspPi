b148e595045b3b2967c659c13af79d6b9cc937db
virtualarchitectures <virtualarchitectures@gmail.com>
2021-08-12 22:45:23 +0100

    Added face counter callback

--- OpenMV/rpc_remote.py
+++ OpenMV/rpc_remote.py
@@ -18,7 +18,7 @@ interface = rpc.rpc_usb_vcp_slave()
 # Call Backs
 ################################################################
 
-# Helper methods used by the call backs below.
+# Helper method to draw frames around detected features
 def draw_detections(img, dects):
     for d in dects:
         c = d.corners()
@@ -26,16 +26,7 @@ def draw_detections(img, dects):
         for i in range(l): img.draw_line(c[(i+0)%l] + c[(i+1)%l], color = (0, 255, 0))
         img.draw_rectangle(d.rect(), color = (255, 0, 0))
 
-# Remote control works via call back methods that the controller
-# device calls via the rpc module on this device. Call backs
-# are functions which take a bytes() object as their argument
-# and return a bytes() object as their result. The rpc module
-# takes care of moving the bytes() objects across the link.
-# bytes() may be the micropython int max in size.
-
-# When called returns x, y, w, and h of the largest face within view.
-#
-# data is unused
+# Function returns x, y, w, and h of the largest face within view.
 def face_detection(data):
     sensor.set_pixformat(sensor.GRAYSCALE)
     sensor.set_framesize(sensor.QVGA)
@@ -45,18 +36,25 @@ def face_detection(data):
     out_face = max(faces, key = lambda f: f[2] * f[3])
     return struct.pack("<HHHH", out_face[0], out_face[1], out_face[2], out_face[3])
 
-# When called returns if there's a "person" or "no_person" within view.
-#
-# data is unused
+# Function returns a "person" or "no_person" within view.
 def person_detection(data):
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
     scores = tf.classify("person_detection", sensor.snapshot())[0].output()
     return ['unsure', 'person', 'no_person'][scores.index(max(scores))].encode()
 
+# Function returns count of faces.
+def face_counter(data):
+    sensor.set_pixformat(sensor.GRAYSCALE)
+    sensor.set_framesize(sensor.QVGA)
+    faces = sensor.snapshot().gamma_corr(contrast=1.5).find_features(image.HaarCascade("frontalface"))
+    if not faces: return bytes() # No detections.
+    return str(len(faces)).encode()
+
 # Register call backs.
 interface.register_callback(face_detection)
 interface.register_callback(person_detection)
+interface.register_callback(face_counter)
 
 # Start processing remote events. Interface.loop() does not return.
 interface.loop()
